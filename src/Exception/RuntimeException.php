7c40f9f10e39f6ea23143f148a92dffae50e03f5
Alejandro Celaya <alejandro@alejandrocelaya.com>
2019-08-10 23:30:47 +0200

    Moved RuntimeException to IpGeolocation module

---
+++ src/Exception/RuntimeException.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace Shlinkio\Shlink\IpGeolocation\Exception;
+
+use RuntimeException as SplRuntimeException;
+
+class RuntimeException extends SplRuntimeException implements ExceptionInterface
+{
+}
